414b3ba02502ad0e102c4cbdfd95e1ed3d71d7ee
Elahe Jalalpour <el.jalalpour@gmail.com>
2016-01-01 11:23:48 +0330

    old-src has been removed.

--- old-src/sdvpn/adapters/__init__.py
+++
@@ -1,9 +0,0 @@
-# In The Name Of God
-# ========================================
-# [] File Name : __init__.py
-#
-# [] Creation Date : 03-10-2015
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
-__author__ = 'Parham Alvani'
